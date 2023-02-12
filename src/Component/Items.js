f3347bb921e15cce2b340acfd1f814117cacc473
Neetu Seharawat <neetumorbca@gmail.com>
2023-02-12 15:27:43 +0530

    ProductList

---
+++ src/Component/Items.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Items = ({products}) => {
+
+  return <div>
+      {products.map(product => (
+        <div key={product.title}>
+          
+           <h3>{product.title}</h3>
+           <img src={product.imageUrl} />
+           <p> Price: {product.price}</p>
+        </div>
+      )) }
+  </div>;
+};
+
+export default Items;
